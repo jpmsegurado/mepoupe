1503f2be420b7f6c3e92b7fb272a8efbf0952856
JP Segurado <jpmsegurado@gmail.com>
2020-01-13 23:40:08 -0300

    feat(home): add dont mess card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
-  <page title="Home">
-    <p>Total despesas: {{ $formatMoney(totalExpenses) }}</p>
-    <p>Total rendas: {{ $formatMoney(totalIncomes) }}</p>
-    <p>Total a investir {{ $formatMoney(totalToInvest) }}</p>
-    <p>Educação: {{ $formatMoney(education) }}</p>
-    <p>Dívidas: {{ $formatMoney(installments) }}</p>
-    <p>Lazer: {{ $formatMoney(fun) }}</p>
+  <page title="Seus números" subtitle="Uma visão gerão de como está sua vida financeira" class="home">
+    <el-row :gutter="24">
+      <el-col v-for="item in items" :key="item.label" :sm="24" :md="12" :lg="8">
+        <el-card class="home__card">
+          <div slot="header" class="home__card__header">
+            <strong>{{ item.label }}</strong>
+          </div>
+          <div class="home__card__value">
+            {{ $formatMoney(item.value) }}
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
   </page>
 </template>
 
@@ -36,7 +42,50 @@ export default {
     },
     installments () {
       return this.totalIncomes * this.configs.installments
+    },
+    items () {
+      return [
+        { label: 'Total rendas', value: this.totalIncomes },
+        { label: 'Total despesas', value: this.totalExpenses },
+        { label: 'Total a investir', value: this.totalToInvest },
+        { label: 'Educação', value: this.education },
+        { label: 'Dívidas', value: this.installments },
+        { label: 'Lazer', value: this.fun },
+        { label: 'Não se meta', value: this.totalIncomes * this.configs.dontMess }
+      ]
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.home {
+  &__card {
+    margin-bottom: 20px;
+
+    &__value {
+      font-size: 35px;
+    }
+
+    &__header {
+      display: flex;
+      align-items: center;
+
+      &__actions {
+        margin-left: auto;
+
+        .el-button {
+          font-size: 20px;
+          height: 15px;
+          padding: 0;
+          color: $primary;
+
+          i {
+            color: $primary;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
